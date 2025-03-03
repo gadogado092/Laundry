576933a869d279e2f81d253763f4afaed468dbc7
rabiuldien <gadogado092@gmail.com>
2025-03-03 11:40:50 +0800

    basic UI

--- app/src/main/java/amat/laundry/ui/screen/transaction/AddTransactionActivity.kt
+++ app/src/main/java/amat/laundry/ui/screen/transaction/AddTransactionActivity.kt
@@ -1,27 +1,62 @@
 package amat.laundry.ui.screen.transaction
 
+import amat.laundry.R
+import amat.laundry.ui.theme.Blue
+import amat.laundry.ui.theme.FontWhite
+import amat.laundry.ui.theme.GreenDark
 import amat.laundry.ui.theme.LaundryAppTheme
+import amat.laundry.ui.theme.TealGreen
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ShoppingBasket
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
 
-class AddTransactionActivity: ComponentActivity() {
+class AddTransactionActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val context = LocalContext.current
-            LaundryAppTheme   {
+            LaundryAppTheme {
                 AddTransactionScreen(context = context)
             }
         }
@@ -44,9 +79,65 @@ fun AddTransactionScreen(
         TopAppBar(
             title = {
                 Text(
-                    text="ada"
+                    text = stringResource(id = R.string.title_add_transaction),
+                    color = FontWhite,
+                    fontSize = 22.sp
                 )
+            },
+            backgroundColor = GreenDark,
+            navigationIcon = {
+                IconButton(
+                    onClick = {
+                        val activity = (context as? Activity)
+                        activity?.finish()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
+                }
             }
         )
+        Column(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .weight(1F)
+                    .fillMaxWidth()
+                    .background(Color.Red)
+            ) {
+                Text("center")
+            }
+
+            Card(
+                modifier = Modifier
+                    .padding(4.dp, 8.dp)
+                    .fillMaxWidth(),
+                colors = CardDefaults.cardColors(Blue),
+                border = BorderStroke(0.5.dp, Blue),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text("3 Layanan", color = Color.White, fontSize = 16.sp)
+                    Row {
+                        Text("30.000", color = Color.White, fontSize = 16.sp)
+                        Spacer(Modifier.width(2.dp))
+                        Image(
+                            imageVector = Icons.Default.ShoppingBasket,
+                            contentDescription = "",
+                            colorFilter = ColorFilter.tint(
+                                Color.White
+                            ),
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
